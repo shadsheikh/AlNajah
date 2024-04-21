d6f44b2f08340afbd512f99fca298fe6fd4d9a9e
Shad Sheikh <shadsheikh3107@gmail.com>
2024-04-21 23:22:35 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,46 @@
+
+import React, { Component } from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Navbar from './components/navbar/Navbar'
+import Footer from './components/footer/Footer'
+import Landing from './components/landing/Landing'
+import About from './components/about/About'
+import Blogs from './components/blogs/Blogs'
+import Sb from './components/scrollbtn/Sb'
+import Courses from './components/courses/Courses'
+import Contact from './components/contact/Contact'
+import Notfound from './components/notfound/Notfound'
+
+
+class App extends Component {
+  render() {
+    return (
+      <BrowserRouter>
+        <>
+          <Sb />
+          <div className='_navbar'>
+            <Navbar />
+          </div>
+
+          <div className='_body'>
+            <Routes>
+              <Route exact path='/' element={<Landing />} />
+              <Route path='/OpenSchool' element={<Landing />} />
+              <Route path='/about' element={<About />} />
+              <Route path='/blogs/*' element={<Blogs />} />
+              <Route path='/courses/*' element={<Courses />} />
+              <Route path='/contact' element={<Contact />} />
+              <Route path='*' element={<Notfound />} />
+            </Routes>
+          </div>
+
+          <div className='_footer'>
+            <Footer />
+          </div>
+
+        </>
+      </BrowserRouter>
+    )
+  }
+}
+export default App
---
+++ src/components/landing/stats/Data.js
@@ -0,0 +1,17 @@
+
+let stats = [
+	{
+		name: 'courses',
+		number: '60'
+	},
+	{
+		name: 'students',
+		number: '1500'
+	},
+	{
+		name: 'followers',
+		number: '20,000'
+	},
+]
+
+export {stats}
