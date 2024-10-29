adf8158fe0cb27788e53c4aec5fc233a2e899f6f
Shad Sheikh <shadsheikh3107@gmail.com>
2024-10-29 16:39:26 +0530

    testmonial

--- src/components/landing/testimonials/Data.js
+++ src/components/landing/testimonials/Data.js
@@ -5,18 +5,21 @@ import img3 from '../../../images/tes/3.jpg';
 const tes = [
 	{
 		img: img1,
-		name: 'alfredo traviski',
-		skill: 'UI/UX Designer'
+		name: 'Bilal',
+		skill: 'England',
+		review: 'My Son Qasim has been learning with Shaykh Abu Safwan for 2 years and his teaching is amazing. We are  extremely happy with him and recommended to your children.'
 	},
 	{
 		img: img2,
-		name: 'André gohan',
-		skill: 'Full Stack Developer'
+		name: 'Umme Aaizah',
+		skill: 'Canada',
+		review: 'My daughter has been learning online with Shaykh Abu Safwan and his teaching techniques have been amazing. His ability to adjust his teaching style based on the child\'s age has been wonderful, extremely happy and recommended to your children'
 	},
 	{
 		img: img3,
-		name: 'gilberto Mariano',
-		skill: 'Ex Student'
+		name: 'Umme Ibrahim',
+		skill: 'Saudi Arabia',
+		review: 'I\'m so delighted to tell you that my child is capable of reading Quran and Arabic language well without the help of anyone.'
 	}
 ];
 
